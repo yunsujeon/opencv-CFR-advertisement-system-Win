fb9d090635ab06cadb598ea0c22e19adc97a11ba
yunsujeon <dbstndi6316@naver.com>
2020-05-24 01:09:57 +0900

    speech recognition 반응속도개선

--- opencv-CFR-advertisement-system-Win.py
+++ opencv-CFR-advertisement-system-Win.py
@@ -38,29 +38,42 @@ print (width)
 print (height)
 pygame.init() #라이브러리 초기화 안해줘도 되긴함
 
-def recognize_speech_from_mic(recognizer, microphone): #stt함수
+
+def recognize_speech_from_mic(recognizer, microphone):
+
+    # check that recognizer and microphone arguments are appropriate type
     if not isinstance(recognizer, sr.Recognizer):
         raise TypeError("`recognizer` must be `Recognizer` instance")
+
     if not isinstance(microphone, sr.Microphone):
         raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
     with microphone as source:
-        recognizer.adjust_for_ambient_noise(source) # #  analyze the audio source for 1 second
+        recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
 
+    # set up the response object
     response = {
         "success": True,
         "error": None,
         "transcription": None
     }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
     try:
         response["transcription"] = recognizer.recognize_google(audio)
     except sr.RequestError:
         # API was unreachable or unresponsive
         response["success"] = False
-        response["error"] = "API unavailable/unresponsive"
+        response["error"] = "API unavailable"
     except sr.UnknownValueError:
         # speech was unintelligible
         response["error"] = "Unable to recognize speech"
+
     return response
 
 def facerecog(faceposes, agelens, firstages, facegenders):
@@ -259,7 +272,7 @@ cv2.destroyAllWindows()
 
 
 
-#
+
 # # 음성인식 주기를 줄였다.
 # def recognize_speech_from_mic(recognizer, microphone):
 #     if not isinstance(recognizer, sr.Recognizer):
