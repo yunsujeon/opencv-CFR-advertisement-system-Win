49c2d026b65cff696bb442ea63e41a214bd99209
TyoRan <38074706+TyoRan@users.noreply.github.com>
2020-05-25 04:07:16 +0900

    update under 10 and child
    update under 10 and child

--- choose_many_people.py
+++ choose_many_people.py
@@ -347,9 +347,15 @@ while True:
 						# print("감지된 얼굴의 첫번째 나이대는 {}0대 입니다.".format(firstage))
 						# print("감지된 얼굴의 두번째 나이대는 {}0대 입니다.".format(secondage))
 
+						# get average age of each person	
 						average_age = []
 						for i in range(faceCount):
-							average_age.append(int(faceage[i][0] + faceage[i][1]) + 2)
+							# age is over 10
+							if len(faceage[i]) == 5:
+								average_age.append(int(faceage[i][0] + faceage[i][1]) + 2)
+							# age is under 10
+							else:
+								average_age.append(int(faceage[i][0]) + 2)
 							print(average_age[i])
 
 						male = [0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -373,7 +379,9 @@ while True:
 									male[6] += 1
 								elif 70 <= average_age[i] < 80:
 									male[7] += 1
-							else:
+								else:
+									male[8] += 1
+							elif facegender[i] == "female":
 								if 1 <= average_age[i] < 10:
 									female[0] += 1
 								elif 10 <= average_age[i] < 20:
@@ -390,6 +398,36 @@ while True:
 									female[6] += 1
 								elif 70 <= average_age[i] < 80:
 									female[7] += 1
+								else:
+									female[8] += 1
+							else:	# gender is nethier male nor female, equal weiht
+								if 1 <= average_age[i] < 10:
+									male[0] += 0.5
+									female[0] += 0.5
+								elif 10 <= average_age[i] < 20:
+									male[1] += 0.5
+									female[1] += 0.5
+								elif 20 <= average_age[i] < 30:
+									male[2] += 0.5
+									female[2] += 0.5
+								elif 30 <= average_age[i] < 40:
+									male[3] += 0.5
+									female[3] += 0.5			
+								elif 40 <= average_age[i] < 50:
+									male[4] += 0.5
+									female[4] += 0.5
+								elif 50 <= average_age[i] < 60:
+									male[5] += 0.5
+									female[5] += 0.5
+								elif 60 <= average_age[i] < 70:
+									male[6] += 0.5
+									female[6] += 1
+								elif 70 <= average_age[i] < 80:
+									male[7] += 0.5
+									female[7] += 0.5
+								else:
+									male[8] += 0.5
+									female[8] += 0.5
 
 						max_male = 0
 						max_female = 0
